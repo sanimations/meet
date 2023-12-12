48f854b535c4e0a6ddd2d0399fd5b31cb508f732
sanimations <smartucci157@gmail.com>
2023-12-12 16:41:03 -0500

    added charts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import NumberOfEvents from './components/NumberOfEvents';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import {extractLocations, getEvents} from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
@@ -42,6 +44,10 @@ const App = () => {
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
+      <div className="charts-container">
+        <EventGenresChart events={events}/>
+        <CityEventsChart allLocations={allLocations} events={events}/>
+      </div>
       <EventList events={events}/>
     </div>
   );
